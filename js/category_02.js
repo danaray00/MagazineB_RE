2ed3a5eb5a15f591e4a7c14b4d745563640d64aa
danaray00 <danaray1004@gmail.com>
2024-02-15 14:14:02 +0900

    2024-02-15
    카테고리 적용(shop/books)

--- js/category_02.js
+++ js/category_02.js
@@ -1,19 +1,19 @@
      // 초기에 'Magazine B' 카테고리에 active 스타일 적용
-     var activeSection = $('.left_category.active').data('contents');
+     var activeSection = $('.left_category_02.active').data('contents');
      $('#' + activeSection).show();
  
-     $(document).on('click', '.left_category', function() {
+     $(document).on('click', '.left_category_02', function() {
          var selectedSection = $(this).data('contents');
  
          // 모든 섹션 숨기기
-         $('.shop_right').hide();
+         $('.books_right').hide();
  
          // 선택된 섹션 표시 및 활성화 처리
          $('#' + selectedSection).show();
          activeSection = selectedSection;
  
          // 모든 카테고리의 강조 효과 제거
-         $('.left_category').removeClass('active');
+         $('.left_category_02').removeClass('active');
  
          // 현재 클릭한 카테고리에 강조 효과 추가
          $(this).addClass('active');
